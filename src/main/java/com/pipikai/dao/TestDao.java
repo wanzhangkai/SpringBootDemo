e6d857df865880357523bca5651ea5ca5fb7d99c
wanzhangkai <wanzhangkai@foxmail.com>
2018-10-23 18:35:32 +0800

    add

--- src/main/java/com/pipikai/dao/TestDao.java
+++ src/main/java/com/pipikai/dao/TestDao.java
@@ -1,13 +1,10 @@
 package com.pipikai.dao;
 
 import com.pipikai.domain.TestObject;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 public interface TestDao {
 
     TestObject getTestObject(Integer id);
-
 
 
 }
