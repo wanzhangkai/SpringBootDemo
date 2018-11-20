daa9ba1c69647a6ecf1dbc02970428d62aec744b
wanzhangkai <wanzhangkai@foxmail.com>
2018-11-20 12:42:48 +0800

    add

---
+++ src/main/java/com/pipikai/design/principle/openclose/JavaCourse.java
@@ -0,0 +1,36 @@
+package com.pipikai.design.principle.openclose;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by wanzhangkai
+ * 2018/11/20 10:43
+ * Email: zhangkaiwan@qq.com
+ */
+public class JavaCourse implements Course {
+
+    private Integer id;
+    private String name;
+    private BigDecimal price;
+
+    public JavaCourse(Integer id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/com/pipikai/design/principle/openclose/JavaDiscountCourse.java
@@ -0,0 +1,24 @@
+package com.pipikai.design.principle.openclose;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by wanzhangkai
+ * 2018/11/20 11:01
+ * Email: zhangkaiwan@qq.com
+ */
+public class JavaDiscountCourse extends JavaCourse {
+
+    public JavaDiscountCourse(Integer id, String name, BigDecimal price) {
+        super(id, name, price);
+    }
+
+    public BigDecimal getOriginPrice() {
+        return super.getPrice();
+    }
+
+    public BigDecimal getPrice() {
+        return super.getPrice().multiply(new BigDecimal("0.8"));
+    }
+
+}
