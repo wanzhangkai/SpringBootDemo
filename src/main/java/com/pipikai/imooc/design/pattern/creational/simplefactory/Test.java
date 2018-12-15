8029c664a462911fdb4e2a064b71e90b7d6b20ed
wanzhangkai <wanzhangkai@foxmail.com>
2018-12-15 12:44:29 +0800

    add

--- src/main/java/com/pipikai/imooc/design/pattern/creational/simplefactory/Test.java
+++ src/main/java/com/pipikai/imooc/design/pattern/creational/simplefactory/Test.java
@@ -8,6 +8,8 @@ package com.pipikai.imooc.design.pattern.creational.simplefactory;
 public class Test {
 
     public static void main(String[] args) {
-        
+        VideoFactory videoFactory = new VideoFactory();
+        Video video = videoFactory.getVideo(JavaVideo.class);
+        video.produce();
     }
 }
