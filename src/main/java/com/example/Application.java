8218368db8a56fb6ed399b9fe4e84d2d79201966
wanzhangkai <wanzhangkai@foxmail.com>
2018-12-24 16:05:49 +0800

    add

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -13,7 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @Slf4j
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("Springboot启动完毕");
