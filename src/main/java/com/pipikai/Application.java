bccca2933b56bfc53045b634dd566f9a325c2acc
wanzhangkai <wanzhangkai@foxmail.com>
2018-12-24 16:31:40 +0800

    add

--- src/main/java/com/pipikai/Application.java
+++ src/main/java/com/pipikai/Application.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.pipikai;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/pipikai/ApplicationTest.java
+++ src/test/java/com/pipikai/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.pipikai;
 
 
 import org.junit.Test;
