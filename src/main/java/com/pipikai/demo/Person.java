f1d12e7826ffe8eec07e1ed5b49150530589f571
wanzhangkai <wanzhangkai123@gmail.com>
2019-01-17 17:07:34 +0800

    更新

---
+++ src/main/java/com/pipikai/demo/Person.java
@@ -0,0 +1,25 @@
+package com.pipikai.demo;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: wanzhangkai
+ * @Email: zhangkaiwan@qq.com
+ * @Date: 2019/1/17 16:33
+ */
+@Slf4j
+@Data
+public class Person {
+
+    private String name;
+    private String name2;
+    private Integer age;
+
+    public Person(String name, String name2, Integer age) {
+        this.name = name;
+        this.name2 = name2;
+        this.age = age;
+    }
+
+}
--- src/main/java/com/pipikai/demo/TestDemo.java
+++ src/main/java/com/pipikai/demo/TestDemo.java
@@ -1,18 +1,29 @@
 package com.pipikai.demo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestDemo {
 
     private static void test1() {
-        String s = "wanzhang";
-        String s1 = "12";
-        String s2 = "11";
-        String s3 = "10";
-        String s4 = "10121231231312321321323";
-        System.out.println(s.hashCode());
-        System.out.println(s1.hashCode());
-        System.out.println(s2.hashCode());
-        System.out.println(s3.hashCode());
-        System.out.println(s4.hashCode());
+        Set<Person> set = new HashSet<Person>();
+        Person p1 = new Person("唐僧","pwd1",25);
+        Person p2 = new Person("孙悟空","pwd2",26);
+        Person p3 = new Person("猪八戒","pwd3",27);
+        set.add(p1);
+        set.add(p2);
+        set.add(p3);
+        System.out.println("总共有:"+set.size()+" 个元素!"); //结果：总共有:3 个元素!
+        p3.setAge(2); //修改p3的年龄,此时p3元素对应的hashcode值发生改变
+
+        set.remove(p3); //此时remove不掉，造成内存泄漏
+
+        set.add(p3); //重新添加，居然添加成功
+        System.out.println("总共有:"+set.size()+" 个元素!"); //结果：总共有:4 个元素!
+        for (Person person : set)
+        {
+            System.out.println(person);
+        }
     }
 
     public static void main(String[] args) {
