78091833b194bd540fcf1624b5814026785cb9af
wanzhangkai <wanzhangkai123@gmail.com>
2019-01-18 16:33:08 +0800

    更新

---
+++ src/main/java/com/pipikai/demo/sortDemo/BubblingSort.java
@@ -0,0 +1,36 @@
+package com.pipikai.demo.sortDemo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: wanzhangkai
+ * @Email: zhangkaiwan@qq.com
+ * @Date: 2019/1/18 16:17
+ */
+@Slf4j
+public class BubblingSort {
+
+    public static void sort(int[] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                int temp;
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] t = {2, 3, 4, 1, 5};
+        sort(t);
+        for (int i1 : t) {
+            System.out.print(i1 + " ");
+        }
+    }
+
+}
